52b3ccbb9ca66db5ad563ea2422d7213755f2c43
khaiCodes <khaicodes99@gmail.com>
2021-01-26 16:16:32 +0800

    implemented json-server in project

--- src/Home.js
+++ src/Home.js
@@ -1,25 +1,29 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import Bloglist from "./Bloglist";
 
 const Home = () => {
-  const [blogs, setBlogs] = useState([
-    { title: "My new website", body: "lorem ipsum...", author: "mario", id: 1 },
-    { title: "Welcome party!", body: "lorem ipsum...", author: "yoshi", id: 2 },
-    {
-      title: "Web dev top tips",
-      body: "lorem ipsum...",
-      author: "mario",
-      id: 3,
-    },
-  ]);
+  const [blogs, setBlogs] = useState(null);
+
+  const handleDelete = (id) => {
+    const newBlogs = blogs.filter((blog) => blog.id !== id);
+    setBlogs(newBlogs);
+  };
+
+  useEffect(() => {
+    fetch("http://localhost:8000/blogs")
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        setBlogs(data);
+      });
+  }, []);
 
   return (
     <div className="home">
-      {blogs.map((blog) => (
-        <div className="blog-preview" key={blog.id}>
-          <h2>{blog.title}</h2>
-          <p>Written by {blog.author}</p>
-        </div>
-      ))}
+      {blogs && (
+        <Bloglist blogs={blogs} title="All blogs" handleDelete={handleDelete} />
+      )}
     </div>
   );
 };
