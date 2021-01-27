1051e5a329525d82d034657f230caf36d65f2e28
khaiCodes <khaicodes99@gmail.com>
2021-01-27 18:47:46 +0800

    finished tut26

--- src/Home.js
+++ src/Home.js
@@ -1,29 +1,16 @@
-import { useState, useEffect } from "react";
 import Bloglist from "./Bloglist";
+import useFetch from "./useFetch";
 
 const Home = () => {
-  const [blogs, setBlogs] = useState(null);
-
-  const handleDelete = (id) => {
-    const newBlogs = blogs.filter((blog) => blog.id !== id);
-    setBlogs(newBlogs);
-  };
-
-  useEffect(() => {
-    fetch("http://localhost:8000/blogs")
-      .then((res) => {
-        return res.json();
-      })
-      .then((data) => {
-        setBlogs(data);
-      });
-  }, []);
+  const { data: blogs, isPending, error } = useFetch(
+    "http://localhost:8000/blogs"
+  );
 
   return (
     <div className="home">
-      {blogs && (
-        <Bloglist blogs={blogs} title="All blogs" handleDelete={handleDelete} />
-      )}
+      {error && <div>{error}</div>}
+      {isPending && <div>loading...</div>}
+      {blogs && <Bloglist blogs={blogs} title="All blogs" />}
     </div>
   );
 };
